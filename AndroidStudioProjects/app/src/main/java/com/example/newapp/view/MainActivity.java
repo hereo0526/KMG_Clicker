426e8342c3c16afec3b11f93a84eb0e738084bda
hereo0526 <hereo0526@gmail.com>
2022-03-26 14:36:44 +0900

    compile failed..

--- AndroidStudioProjects/app/src/main/java/com/example/newapp/view/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/example/newapp/view/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 //import android.widget.Button;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
