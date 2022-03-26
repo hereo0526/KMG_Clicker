637bc07e5ccd7fa6e0346f210be4f85aee5eeeba
hereo0526 <hereo0526@gmail.com>
2022-03-26 14:38:38 +0900

    fixed error
    wow~

--- AndroidStudioProjects/app/src/main/java/com/example/newapp/view/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/example/newapp/view/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-//import android.widget.Button;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
