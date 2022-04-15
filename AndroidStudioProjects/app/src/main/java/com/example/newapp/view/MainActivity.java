447c4c7dcbfdeaabd62b1609d6a3624a30ac113c
hereo0526 <hereo0526@gmail.com>
2022-04-15 18:33:11 +0900

    error fixed
    +SharedPreferences

--- AndroidStudioProjects/app/src/main/java/com/example/newapp/view/MainActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/example/newapp/view/MainActivity.java
@@ -33,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        image_click = findViewById(R.id.image_click);
+        score_text = findViewById(R.id.score_text);
+        double_need = findViewById(R.id.double_need);
+        click_add = findViewById(R.id.click_add);
+        button_add = findViewById(R.id.button_add);
+        button_clear = findViewById(R.id.button_clear);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.pop);
+
         SharedPreferences pref_get = getSharedPreferences("preferences", MODE_PRIVATE);
         if( (pref_get != null) && (pref_get.contains("score")) ) {
             int saved_score = pref_get.getInt("score", 0);
@@ -42,22 +51,14 @@ public class MainActivity extends AppCompatActivity {
         if( (pref_get != null) && (pref_get.contains("increase")) ) {
             int saved_increase = pref_get.getInt("increase", 1);
             presenter.setIncrease(saved_increase);
-            setTextIncrease(Integer.toString(saved_increase));
+            String str_saved_increase = Integer.toString(saved_increase);
+            setTextIncrease("("+'+'+str_saved_increase+')');
         }
         if( (pref_get != null) && (pref_get.contains("double_need")) ) {
             int saved_double_need = pref_get.getInt("double_need", 10);
             presenter.setDoubleNeed(saved_double_need);
             setTextDoubleNeed(Integer.toString(saved_double_need));
         }
-
-        image_click = findViewById(R.id.image_click);
-        score_text = findViewById(R.id.score_text);
-        double_need = findViewById(R.id.double_need);
-        click_add = findViewById(R.id.click_add);
-        button_add = findViewById(R.id.button_add);
-        button_clear = findViewById(R.id.button_clear);
-
-        animation = AnimationUtils.loadAnimation(this, R.anim.pop);
 
         image_click.setOnClickListener(new View.OnClickListener() {
             @Override
